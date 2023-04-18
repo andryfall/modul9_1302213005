c3e18eb0eddf8a91abada1d2888d92ac55573f85
andryfall <andryfall@student.telkomuniversity.ac.id>
2023-04-18 16:19:45 +0700

    Menambahkan API Mahasiswa

---
+++ Mahasiswa.cs
@@ -0,0 +1,19 @@
+﻿namespace modul9_1302213005
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public int NIM { get; set; }
+
+        public List<string> Courses { get; set; }
+
+        public int Year { get; set; }
+        public Mahasiswa(string nama, int nim, List<string> courses, int year)
+        {
+            this.Nama = nama;
+            this.NIM = nim;
+            Courses = courses;
+            Year = year;
+        }
+    }
+}
